11710181cd9533cb8159c3c852ecc1cea4acfce3
hoangminhbao8102 <90952548+hoangminhbao8102@users.noreply.github.com>
2023-09-24 21:27:26 +0700

    main

---
+++ BaiTapLab/ASP.NET CORE/TipsAndTricks/TatBlog.Services/Blogs/BlogRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TatBlog.Core.Entities;
+
+namespace TatBlog.Services.Blogs
+{
+    public class BlogRepository : IBlogRepository
+    {
+        public Task<IList<Post>> GetPopularArticlesAsync(int numPosts, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post> GetPostAsync(int year, int month, string slug, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task IncreaseViewCountAsync(int postId, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsPostSlugExistedAsync(int postId, string slug, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BaiTapLab/ASP.NET CORE/TipsAndTricks/TatBlog.WinApp/Program.cs
+++ BaiTapLab/ASP.NET CORE/TipsAndTricks/TatBlog.WinApp/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TatBlog.Data.Contexts;
+using TatBlog.Data.Seeders;
+
+var context = new BlogDbContext();
+
+var seeder = new DataSeeder(context);
+
+seeder.Initialize();
+
+var authors = context.Authors.ToList();
+
+var posts = context.Posts
+    .Where(p => p.Published)
+    .OrderBy(p => p.Title)
+    .Select(p => new
+    {
+        Id = p.Id,
+        Title = p.Title,
+        ViewCount = p.ViewCount,
+        PostedDate = p.PostedDate,
+        Author = p.Author.FullName,
+        Category = p.Category.Name
+    })
+    .ToList();
+
+Console.WriteLine("{0,-4}{1,-30}{2,-30}{3,12}", "ID", "Full Name", "Email", "Joined Date");
+
+foreach (var author in authors)
+{
+    Console.WriteLine("{0,-4}{1,-30}{2,-30}{3,12:MM/dd/yyyy}", author.Id, author.FullName, author.Email, author.JoinedDate);
+}
+
+foreach (var post in posts)
+{
+    Console.WriteLine("ID      : {0}",post.Id);
+    Console.WriteLine("Title   : {0}",post.Title);
+    Console.WriteLine("View    : {0}",post.ViewCount);
+    Console.WriteLine("Date    : {0:MM/dd/yyyy}",post.PostedDate);
+    Console.WriteLine("Author  : {0}", post.Author);
+    Console.WriteLine("Category: {0}", post.Category);
+    Console.WriteLine("".PadRight(80, '-'));
+}
