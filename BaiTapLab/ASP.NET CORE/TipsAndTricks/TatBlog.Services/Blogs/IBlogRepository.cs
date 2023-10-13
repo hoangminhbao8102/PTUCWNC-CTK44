87e8b5575d60ecf49a0dff524d068d3bf1b58e1b
hoangminhbao8102 <90952548+hoangminhbao8102@users.noreply.github.com>
2023-10-13 20:16:23 +0700

    main

--- BaiTapLab/ASP.NET CORE/TipsAndTricks/TatBlog.Services/Blogs/IBlogRepository.cs
+++ BaiTapLab/ASP.NET CORE/TipsAndTricks/TatBlog.Services/Blogs/IBlogRepository.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TatBlog.Core.Contracts;
+using TatBlog.Core.DTO;
 using TatBlog.Core.Entities;
 
 namespace TatBlog.Services.Blogs
@@ -16,5 +18,9 @@ namespace TatBlog.Services.Blogs
         Task<bool> IsPostSlugExistedAsync(int postId, string slug, CancellationToken cancellationToken = default);
 
         Task IncreaseViewCountAsync(int postId, CancellationToken cancellationToken= default);
+
+        Task<IList<CategoryItem>> GetCategoriesAsync(bool showOnMenu = false, CancellationToken cancellationToken = default);
+
+        Task<IPagedList<TagItem>> GetPagedTagsAsync(IPagingParams pagingParams, CancellationToken cancellationToken = default);
     }
 }
--- BaiTapLab/ASP.NET CORE/TipsAndTricks/TatBlog.WinApp/Program.cs
+++ BaiTapLab/ASP.NET CORE/TipsAndTricks/TatBlog.WinApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using TatBlog.Data.Contexts;
 using TatBlog.Data.Seeders;
+using TatBlog.Services.Blogs;
+using TatBlog.WinApp;
 
 var context = new BlogDbContext();
 
@@ -9,19 +11,19 @@ seeder.Initialize();
 
 var authors = context.Authors.ToList();
 
-var posts = context.Posts
-    .Where(p => p.Published)
-    .OrderBy(p => p.Title)
-    .Select(p => new
-    {
-        Id = p.Id,
-        Title = p.Title,
-        ViewCount = p.ViewCount,
-        PostedDate = p.PostedDate,
-        Author = p.Author.FullName,
-        Category = p.Category.Name
-    })
-    .ToList();
+//var posts = context.Posts
+//    .Where(p => p.Published)
+//    .OrderBy(p => p.Title)
+//    .Select(p => new
+//    {
+//        Id = p.Id,
+//        Title = p.Title,
+//        ViewCount = p.ViewCount,
+//        PostedDate = p.PostedDate,
+//        Author = p.Author.FullName,
+//        Category = p.Category.Name
+//    })
+//    .ToList();
 
 Console.WriteLine("{0,-4}{1,-30}{2,-30}{3,12}", "ID", "Full Name", "Email", "Joined Date");
 
@@ -29,6 +31,10 @@ foreach (var author in authors)
 {
     Console.WriteLine("{0,-4}{1,-30}{2,-30}{3,12:MM/dd/yyyy}", author.Id, author.FullName, author.Email, author.JoinedDate);
 }
+
+IBlogRepository blogRepo = new BlogRepository(context);
+
+var posts = await blogRepo.GetPopularArticlesAsync(3);
 
 foreach (var post in posts)
 {
@@ -39,4 +45,30 @@ foreach (var post in posts)
     Console.WriteLine("Author  : {0}", post.Author);
     Console.WriteLine("Category: {0}", post.Category);
     Console.WriteLine("".PadRight(80, '-'));
+}
+
+var categories = await blogRepo.GetCategoriesAsync();
+
+Console.WriteLine("{0,-5}{1,-50}{2,10}", "ID", "Name", "Count");
+
+foreach (var item in categories)
+{
+    Console.WriteLine("{0,-5}{1,-50}{2,10}", item.Id, item.Name, item.PostCount);
+}
+
+var pagingParams = new PagingParams
+{
+    PageNumber = 1,
+    PageSize = 5,
+    SortColumn = "Name",
+    SortOrder = "DESC"
+};
+
+var tagList = await blogRepo.GetPagedTagsAsync(pagingParams);
+
+Console.WriteLine("{0,-5}{1,-50}{2,10}", "ID", "Name", "Count");
+
+foreach (var item in tagList)
+{
+    Console.WriteLine("{0,-5}{1,-50}{2,10}", item.Id, item.Name, item.PostCount);
 }
